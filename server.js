9f33cf2d5fa101ddca1e234c14cee14cc10a4149
Isk <142883272+iskroshaf@users.noreply.github.com>
2025-06-22 15:26:46 +0800

    .

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const quran_routes = require('./routes/api/quran_routes');
 const doa_routes = require('./routes/api/doa_routes');
 const prayer_routes = require('./routes/api/prayer_routes');
 const verse_notes_routes = require('./routes/api/verse_notes_routes');
-<<<<<<< HEAD
+
 const point_routes = require('./routes/api/point_routes');
-=======
->>>>>>> e7ae5f4826d27cad412e901b65834bcc383687a1
 const chat_routes = require('./routes/api/chat_routes');
 
 const esp = require('./routes/api/esp_routes');
@@ -32,10 +30,7 @@ app.use('/api', quran_routes);
 app.use('/api', doa_routes);
 app.use('/api', prayer_routes);
 app.use('/api', verse_notes_routes);
-<<<<<<< HEAD
 app.use('/api', point_routes);
-=======
->>>>>>> e7ae5f4826d27cad412e901b65834bcc383687a1
 app.use('/api', chat_routes);
 
 app.use('/api', esp);
